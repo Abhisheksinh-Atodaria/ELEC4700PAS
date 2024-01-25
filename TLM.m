ed35aa1d05ea44ca6f0a3ee3c63936a697c3aee0
Abhisheksinh-Atodaria <abhisheksinhmitulsin@cmail.carleton.ca>
2024-01-25 16:31:33 -0500

    Milestone 1 Code

--- TLM.m
+++ TLM.m
@@ -5,6 +5,7 @@ set(0,'Defaultaxeslinewidth',2)
 
 set(0,'DefaultFigureWindowStyle','docked')
 
+%defining constants
 c_c = 299792458;
 c_eps_0 = 808542149e-12;
 c_eps_0_cm = c_eps_0/100;
@@ -13,29 +14,42 @@ c_q = 1.60217653e-19;
 c_hb = 1.05457266913e-34;
 c_h = c_hb*2*pi;
 
-InputParasL.E0=1e5;
-InputParasL.we = 0;
-InputParasL.t0 = 2e-12;
-InputParasL.wg = 5e-13;
-InputParasL.phi = 0;
-InputParasR = 0;
+RL = 0.9i;
+RR = 0.9i;
 
+%Input parameters 
+InputParasL.E0 = 1e12;
+InputParasL.we = 4e12;
+InputParasL.t0 = 2e-12;
+InputParasL.wg = 100;
+InputParasL.phi = 0;
+InputParasR = 10;
+%InputParasR.E0 = 1e5;
+%InputParasR.we = 10e10;
+%InputParasR.t0 = 2e-12;
+%InputParasR.wg = 10e-10;
+%InputParasR.phi = 0;
+
+%defining the refractive index and wavelength
 n_g = 3.5;
 vg = c_c/n_g*1e2;
 Lambda = 1550e-9;
 
+%simulation setup
 plotN = 10;
 
+%boundry functions
 L = 1000e-6*1e2;
 XL = [0,L];
-YL = [0,InputParasL.E0];
+YL = [-InputParasL.E0,InputParasL.E0];
 
-Nz = 500;
+
+Nz = 100;
 dz = L/(Nz-1);
 dt = dz/vg;
 fsync = dt*vg/dz;
 
-Nt = floor(2*Nz);
+Nt = floor(5*Nz);
 tmax = Nt*dt;
 t_L = dt*Nz;
 
@@ -46,29 +60,37 @@ InputR = nan(1,Nt);
 OutputL = nan(1,Nt);
 OutputR = nan(1,Nt);
 
+%Initializing electric field vectors
 Ef =zeros(size(z));
 Er = zeros(size(z));
 
+%defining the electric field vectors
 Ef1 = @SourceFct;
 ErN = @SourceFct;
 
+%Input functions
 t = 0;
 time(1) = t;
 InputL(1) = Ef1(t,InputParasL);
 InputR(1) = ErN(t,InputParasR);
 
-OutputL(1) = Ef(Nz);
-OutputR(1) = Er(1);
+%output functions
 
+OutputR(1) = Er(1);
+OutputL(1) = Ef(Nz);
+
+
+%plotting the vectors
 Ef(1) = InputL(1);
 Er(Nz) = InputR(1);
+
 figure('name','Fields')
-subplot(3,3,1)
+subplot(3,1,1)
 plot(z*10000,real(Ef),'r');
 hold off
 xlabel('z(\mum)')
 ylabel('E_f')
-subplot(3,2,1)
+subplot(3,1,2)
 plot(z*10000,real(Er),'b');
 xlabel('z(\mum)')
 ylabel('E_r')
@@ -87,18 +109,22 @@ for i = 2:Nt
     t = dt*(i-1);
     time(i) = t;
 
-    InputL(i) = Ef1(t,InputParasL);
-    InputR(i) = ErN(t,0);
+    InputL(i) = Ef1(t,InputParasL);%setting input equal to source fucntion
+    InputR(i) = ErN(t,InputParasR);%setting input equal to source fucntion
 
-    Ef(1) = InputL(i);
-    Er(Nz) = InputR(i);
+    
+    Ef(1) = InputL(i) + RL*Er(1);%setting The electric field vectors equal to the Inputs with the reflectio added
+    Er(Nz) = InputR(i) + RR*Ef(Nz);%setting The electric field vectors equal to the Inputs with the reflectio added
+    
+    
 
     Ef(2:Nz) = fsync*Ef(1:Nz-1);
     Er(1:Nz-1) = fsync*Er(2:Nz);
 
-    OutputR(i) = Ef(Nz);
-    OutputR(i) = Er(1);
+    OutputR(i) = Ef(Nz)*(1-RR);
+    OutputL(i) = Er(1)*(1-RL);
 
+    %plotting Ef
     if mod(i,plotN) == 0
         subplot(3,1,1)
         plot(z*10000,real(Ef),'r'); hold on
@@ -110,8 +136,8 @@ for i = 2:Nt
         legend('\Re','\Im')
         hold off
         subplot(3,1,2)
-        plot(z*10000,real(Ef),'b'); hold on
-        plot(z*10000,imag(Ef),'b--'); hold off
+        plot(z*10000,real(Er),'b'); hold on
+        plot(z*10000,imag(Er),'b--'); hold off
         xlim(XL*1e4)
         ylim(YL)
         xlabel('z(\mum)')
@@ -119,6 +145,7 @@ for i = 2:Nt
         legend('\Re','\Im')
         
         hold off
+        %plotting Er
         subplot(3,1,3)
         plot(time*1e12,real(InputL),'r'); hold on
         plot(time*1e12,real(OutputR),'g');
